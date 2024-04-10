6e669b8dcf0c6170b754cd708600e70af51edb21
Yermek Kapushev <kapushev@gmail.com>
2024-04-10 12:03:51 +0300

    Convert to half when retrieving

--- colbert/modeling/checkpoint.py
+++ colbert/modeling/checkpoint.py
@@ -78,7 +78,7 @@ class Checkpoint(ColBERT):
 
             keep_dims_ = "return_mask" if keep_dims == "flatten" else keep_dims
             batches = [
-                self.doc(input_ids, attention_mask, keep_dims=keep_dims_, to_cpu=to_cpu)
+                self.doc(input_ids, attention_mask, keep_dims=keep_dims_, to_cpu=to_cpu, to_half=True)
                 for input_ids, attention_mask in tqdm(text_batches, disable=not showprogress)
             ]
 
