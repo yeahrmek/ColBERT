328a86cdc618e99e6c1dc3e4f785c5280b14265a
Yermek Kapushev <kapushev@gmail.com>
2024-04-21 21:53:51 +0300

    Fixed mask_punctuation = false

--- colbert/modeling/colbert.py
+++ colbert/modeling/colbert.py
@@ -21,6 +21,7 @@ class ColBERT(BaseColBERT):
 
         ColBERT.try_load_torch_extensions(self.use_gpu)
 
+        self.skiplist = []
         if self.colbert_config.mask_punctuation:
             self.skiplist = {
                 w: True
