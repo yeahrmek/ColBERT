3a8e920b7e358fea2d73c69e6ddab4f339eb48c8
Yermek Kapushev <kapushev@gmail.com>
2024-04-28 15:39:16 +0300

    Copy indexing settings from one config to another

---
+++ colbert/infra/config/utils.py
@@ -0,0 +1,11 @@
+from colbert.infra.config.settings import IndexingSettings, ResourceSettings
+
+
+def copy_essential_config(source_config, target_config):
+    for field in IndexingSettings.__dataclass_fields__:
+        setattr(target_config, field, getattr(source_config, field))
+
+    for field in ResourceSettings.__dataclass_fields__:
+        setattr(target_config, field, getattr(source_config, field))
+
+    target_config.experiment = source_config.experiment
